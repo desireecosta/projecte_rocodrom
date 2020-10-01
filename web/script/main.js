d3442fde49f175858e8bb45cc961e831f930faa0
Alex Cristian Oros <60178770+alexorosubiqum@users.noreply.github.com>
2020-10-01 12:56:25 +0200

    GALERIA Y MERCH FUNCIONAN
    NO TOCAR MÁS

--- web/script/main.js
+++ web/script/main.js
@@ -1,51 +1,56 @@
-//MAKE GALLERY IMAGES LARGER VERSION 2
-// Get the modal
-var modal = document.getElementById("myModal");
+//MODAL GALERIA
 
+// Get the modal
+var modalGaleria = document.getElementById("myModalGaleria");
 // Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementsByClassName("imatge");
-var modalImg = document.getElementById("img01");
+var img = document.getElementsByClassName("imatgeGaleria");
+var modalImgGaleria = document.getElementById("imgModalGaleria");
 var captionText = document.getElementsByClassName("caption");
-console.log(modalImg);
-$(".imatge").click(function () {
-  modal.style.display = "block";
-  modalImg.src = this.src;
+$(".imatgeGaleria").click(function () {
+  modalGaleria.style.display = "block";
+  modalImgGaleria.src = this.src;
   captionText.innerHTML = document.getElementsByClassName("caption");
 });
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var spanGaleria = document.getElementsByClassName("closeGaleria")[0];
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-  modal.style.display = "none";
-};
+// spanGaleria.onclick = function () {
+//   modalGaleria.style.display = "none";
+// };
 
+$(".closeGaleria").click(function () {
+  modalGaleria.style.display = "none";
+});
 
-<<<<<<< Updated upstream
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-//MAKE GALLERY IMAGES LARGER VERSION 2
+//MODAL MERCH
 // Get the modal
-var modal = document.getElementById("MyModal");
+var modalMerch = document.getElementById("myModalMerch");
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementsByClassName("image");
-var modalImgMerch = document.getElementById("img02");
+var img = document.getElementsByClassName("imatgeMerch");
+var modalImgMerch = document.getElementById("imgModalMerch");
 var captionText = document.getElementsByClassName("caption");
-console.log(modalImgMerch);
-$(".imatge").click(function () {
-  modal.style.display = "block";
-  modalImg.src = this.src;
+
+$(".imatgeMerch").click(function () {
+  modalMerch.style.display = "block";
+  modalImgMerch.src = this.src;
   captionText.innerHTML = document.getElementsByClassName("caption");
 });
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("closeMerch")[0];
+var spanMerch = document.getElementsByClassName("closeMerch")[0];
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-  modal.style.display = "none";
-};
+// spanMerch.onclick = function () {
+//   modalMerch.style.display = "none";
+// };
 
-
-
+$(".closeMerch").click(function () {
+  modalMerch.style.display = "none";
+});
