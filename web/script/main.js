97ae9ae7b74ff892923defa1ce586aeac23f2d5e
marina197438 <69304133+marina197438@users.noreply.github.com>
2020-10-01 11:03:43 +0200

    Peleandome con  la pagina

--- web/script/main.js
+++ web/script/main.js
@@ -22,3 +22,28 @@ span.onclick = function () {
 };
 
 ////////////////////  MAPA  ////////////////////
+
+//MAKE GALLERY IMAGES LARGER VERSION 2
+// Get the modal
+var modal = document.getElementById("MyModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementsByClassName("image");
+var modalImgMerch = document.getElementById("img02");
+var captionText = document.getElementsByClassName("caption");
+console.log(modalImgMerch);
+$(".imatge").click(function () {
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = document.getElementsByClassName("caption");
+});
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("closeMerch")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+////////////////////  MAPA  ////////////////////
