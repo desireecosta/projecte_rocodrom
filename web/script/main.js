cd9d05fd3ab2d7e899b82b6abcf6c8386f0141f2
marina197438 <69304133+marina197438@users.noreply.github.com>
2020-10-01 12:57:01 +0200

    Este no es el correcto

--- web/script/main.js
+++ web/script/main.js
@@ -25,25 +25,25 @@ span.onclick = function () {
 
 //MAKE GALLERY IMAGES LARGER VERSION 2
 // Get the modal
-var modal = document.getElementById("MyModal");
+var modalMerch = document.getElementById("MyModal");
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementsByClassName("image");
+var imgMerch = document.getElementsByClassName("image");
 var modalImgMerch = document.getElementById("img02");
-var captionText = document.getElementsByClassName("caption");
+var captionTextMerch = document.getElementsByClassName("captionMerch");
 console.log(modalImgMerch);
-$(".imatge").click(function () {
-  modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = document.getElementsByClassName("caption");
+$(".image").click(function () {
+  modalMerch.style.display = "block";
+  modalImgMerch.src = this.src;
+  captionText.innerHTML = document.getElementsByClassName("captionMerch");
 });
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("closeMerch")[0];
+var spanMerch = document.getElementsByClassName("closeMerch")[0];
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-  modal.style.display = "none";
+spanMerch.onclick = function () {
+  modalMerch.style.display = "none";
 };
 
 ////////////////////  MAPA  ////////////////////
